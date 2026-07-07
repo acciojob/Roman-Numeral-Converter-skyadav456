111276d555171c4f7f733a2d1fc9c9f21b8df3c5
Sharad Kumar <87852635+skyadav456@users.noreply.github.com>
2026-07-07 14:53:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -28,22 +28,3 @@ function convertToRoman(num) {
             num -= value;
         }
     }
-
-    return result;
-}
-
-const input = parseInt(prompt("Enter a number:"), 10);
-alert(convertToRoman(input));
-
-  //your code here
-
-}
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
