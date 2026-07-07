2548c0be84b4b9c94b7d63fb6bf13977883268a6
Sharad Kumar <87852635+skyadav456@users.noreply.github.com>
2026-07-07 14:52:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+
+    const romanNumerals = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ];
+
+    if (num === 0) {
+        return "";
+    }
+
+    let result = "";
+
+    for (const [symbol, value] of romanNumerals) {
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+    }
+
+    return result;
+}
+
+const input = parseInt(prompt("Enter a number:"), 10);
+alert(convertToRoman(input));
 
   //your code here
 
